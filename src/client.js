a68c5eb8423414502b3c7f285db6d722a7f0c205
nmonterroso <nmonterroso@users.noreply.github.com>
2020-03-27 09:27:50 -0700

    better separation, change authWindow to be promise

--- src/client.js
+++ src/client.js
@@ -1,9 +1,14 @@
 const { app } = require('electron')
-const Discord = require('./discord')
-const AuthWindow = require('./discord/authwindow')
+const { AuthWindow, END_STATES } = require('./discord/authwindow')
 const env = require('./env')
 
 app.whenReady().then(() => {
-  const authWindow = new AuthWindow(env.clientId, env.authRedirectUri)
-  new Discord(authWindow).auth()
+  new AuthWindow(env.clientId, env.authRedirectUri)
+    .auth()
+    .then((accessToken) => {
+      console.log(accessToken)
+    })
+    .catch((endState) => {
+      console.error(endState)
+    })
 })
--- src/discord/authwindow.js
+++ src/discord/authwindow.js
@@ -1,50 +1,84 @@
 const { BrowserWindow } = require('electron')
 
+function createWindow() {
+  return new BrowserWindow({
+    width: 600,
+    height: 750,
+    center: true,
+    autoHideMenuBar: true,
+  })
+}
+
+const END_STATES = {
+  ERROR_UNKNOWN: 1,
+  AUTH_CANCELED: 2,
+}
+
 class AuthWindow {
 
   constructor(clientId, redirectUri) {
     this.window = null
+    this.authPromise = null
+    this.authComplete = false
     this.clientId = clientId
     this.redirectUri = redirectUri
   }
 
-  show() {
-    if (this.window === null) {
-      this.createWindow();
+  auth() {
+    if (this.authPromise !== null) {
+      return this.authPromise
+    } else if (this.window === null) {
+      this.window = createWindow();
     }
 
+    this.authPromise = new Promise(((resolve, reject) => {
+      this.window.webContents.on('dom-ready', () => { this.onDomReady(resolve, reject) })
+      this.window.on('close', () => { this.onClose(reject) })
+    }))
+
+    this.loadUrl()
     this.window.show()
+
+    return this.authPromise
   }
 
-  createWindow() {
-    this.window = new BrowserWindow({
-      width: 600,
-      height: 750,
-      center: true,
-      autoHideMenuBar: true,
-    })
-
+  loadUrl() {
     this.window.loadURL(
       'https://discordapp.com/api/oauth2/authorize?'+
       'response_type=code&'+
       `client_id=${this.clientId}&`+
       'scope=identify&'+
       `redirect_uri=${encodeURIComponent(this.redirectUri)}&`+
-      'prompt=none'
+      'prompt=none' // can be consent to force user to reclick authorize button
     )
+  }
 
-    this.window.webContents.on('dom-ready', () => {
-      const current = this.window.webContents.history[this.window.webContents.currentIndex]
-      if (current.startsWith(this.redirectUri)) {
-        this.window.webContents
-          .executeJavaScript('getAccessToken()')
-          .then((jsonString) => JSON.parse(jsonString))
-          .then((accessToken) => {
-            console.log(accessToken)
-          })
-      }
-    })
+  onDomReady(resolve, reject) {
+    const current = this.window.webContents.history[this.window.webContents.currentIndex]
+    if (current.startsWith(this.redirectUri)) {
+      // TODO: check response code, if non-200 then reject(END_STATES.AUTH_CANCELED)
+      this.window.webContents
+        .executeJavaScript('getAccessToken()')
+        .then((jsonString) => JSON.parse(jsonString))
+        .then((accessToken) => {
+          this.authComplete = true
+          resolve(accessToken)
+        })
+        .catch((e) => {
+          console.error(e)
+          reject(END_STATES.ERROR_UNKNOWN)
+        })
+        .finally(() => {
+          this.window.close()
+        })
+    }
+  }
+
+  onClose(reject) {
+    if (!this.authComplete) {
+      reject(END_STATES.AUTH_CANCELED)
+    }
   }
 }
 
-module.exports = AuthWindow
+module.exports = { AuthWindow, END_STATES }
